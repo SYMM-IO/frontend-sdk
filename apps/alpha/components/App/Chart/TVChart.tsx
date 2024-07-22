25eb54946578ada03e01d4db2eab91c409938ba8
Mohammad Ehsan Kaji <mr.ehsan.kaji@gmail.com>
2024-07-22 17:12:20 +0330

    Remove alpha codes

--- apps/alpha/components/App/Chart/TVChart.tsx
+++
@@ -1,8 +0,0 @@
-import { useMemo } from "react";
-import TradingViewWidget from "./TVChart2";
-
-const TVChart = ({ symbol }: { symbol: string }) => {
-  return useMemo(() => <TradingViewWidget symbol={symbol} />, [symbol]);
-};
-
-export default TVChart;
--- apps/alpha/components/ReviewModal/OpenPosition/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as OpenPositionModal } from "./Modal";
